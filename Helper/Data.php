418b33375f850d1ee2e194fc880f99a77c980c60
GARDENLAWN <rpcsky@gmail.com>
2025-12-01 22:10:14 +0100

    Init

--- Helper/Data.php
+++ Helper/Data.php
@@ -7,14 +7,17 @@ use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Encryption\EncryptorInterface;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Customer\Model\Session;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
 
-class Data extends AbstractHelper
+class Data extends AbstractHelper implements ArgumentInterface
 {
     public const string XML_PATH_CEIDG_API_BASE_URL = 'ceidg_api/general/base_url';
     public const string XML_PATH_CEIDG_API_PKD_CODE = 'ceidg_api/general/pkd_code';
     public const string XML_PATH_CEIDG_API_VOIVODESHIP = 'ceidg_api/general/voivodeship';
     public const string XML_PATH_CEIDG_API_COMPANY_STATUS = 'ceidg_api/general/company_status';
     public const string XML_PATH_CEIDG_API_TOKEN = 'ceidg_api/general/api_token';
+    public const string XML_PATH_B2B_CUSTOMER_GROUPS = 'gardenlawn_core/b2b/customer_groups';
 
     /**
      * @var EncryptorInterface
@@ -22,15 +25,23 @@ class Data extends AbstractHelper
     private EncryptorInterface $encryptor;
 
     /**
+     * @var Session
+     */
+    private Session $customerSession;
+
+    /**
      * @param Context $context
      * @param EncryptorInterface $encryptor
+     * @param Session $customerSession
      */
     public function __construct(
         Context $context,
-        EncryptorInterface $encryptor
+        EncryptorInterface $encryptor,
+        Session $customerSession
     ) {
         parent::__construct($context);
         $this->encryptor = $encryptor;
+        $this->customerSession = $customerSession;
     }
 
     /**
@@ -82,5 +93,26 @@ class Data extends AbstractHelper
     {
         $token = $this->scopeConfig->getValue(self::XML_PATH_CEIDG_API_TOKEN);
         return $token ? $this->encryptor->decrypt($token) : null;
+    }
+
+    /**
+     * Get B2B Customer Groups
+     *
+     * @return array
+     */
+    public function getB2bCustomerGroups(): array
+    {
+        $groups = $this->scopeConfig->getValue(self::XML_PATH_B2B_CUSTOMER_GROUPS, ScopeInterface::SCOPE_STORE);
+        return $groups ? explode(',', $groups) : [];
+    }
+
+    /**
+     * Get Current Customer Group Id
+     *
+     * @return int|null
+     */
+    public function getCurrentCustomerGroupId(): ?int
+    {
+        return (int)$this->customerSession->getCustomerGroupId();
     }
 }
