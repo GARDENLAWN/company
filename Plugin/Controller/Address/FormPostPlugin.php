a6253e0413de22a2157f512dde469d37712f8bd7
GARDENLAWN <rpcsky@gmail.com>
2025-12-02 14:49:18 +0100

    Init

--- Plugin/Controller/Address/FormPostPlugin.php
+++ Plugin/Controller/Address/FormPostPlugin.php
@@ -3,15 +3,19 @@ declare(strict_types=1);
 
 namespace GardenLawn\Company\Plugin\Controller\Address;
 
+use Magento\Customer\Api\AddressRepositoryInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\AddressInterface;
+use Magento\Customer\Api\Data\AddressInterfaceFactory;
 use Magento\Customer\Controller\Address\FormPost;
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Message\ManagerInterface;
-use GardenLawn\Company\Helper\Data as CompanyHelper;
-use GardenLawn\Company\Api\Data\CeidgService;
-use Magento\Customer\Api\AddressRepositoryInterface;
-use Magento\Customer\Model\Session;
 use Magento\Framework\UrlFactory;
+use GardenLawn\Company\Api\Data\CeidgService;
+use GardenLawn\Company\Helper\Data as CompanyHelper;
 
 class FormPostPlugin
 {
@@ -21,8 +25,10 @@ class FormPostPlugin
     private ManagerInterface $messageManager;
     private RedirectFactory $resultRedirectFactory;
     private AddressRepositoryInterface $addressRepository;
+    private CustomerRepositoryInterface $customerRepository;
     private Session $customerSession;
     private UrlFactory $urlFactory;
+    private AddressInterfaceFactory $addressFactory;
 
     public function __construct(
         CompanyHelper $companyHelper,
@@ -31,8 +37,10 @@ class FormPostPlugin
         ManagerInterface $messageManager,
         RedirectFactory $resultRedirectFactory,
         AddressRepositoryInterface $addressRepository,
+        CustomerRepositoryInterface $customerRepository,
         Session $customerSession,
-        UrlFactory $urlFactory
+        UrlFactory $urlFactory,
+        AddressInterfaceFactory $addressFactory
     ) {
         $this->companyHelper = $companyHelper;
         $this->ceidgService = $ceidgService;
@@ -40,67 +48,106 @@ class FormPostPlugin
         $this->messageManager = $messageManager;
         $this->resultRedirectFactory = $resultRedirectFactory;
         $this->addressRepository = $addressRepository;
+        $this->customerRepository = $customerRepository;
         $this->customerSession = $customerSession;
         $this->urlFactory = $urlFactory;
+        $this->addressFactory = $addressFactory;
     }
 
+    /**
+     * @throws LocalizedException
+     */
     public function aroundExecute(FormPost $subject, callable $proceed)
     {
-        $isB2bCustomer = in_array($this->companyHelper->getCurrentCustomerGroupId(), $this->companyHelper->getB2bCustomerGroups());
-
-        if (!$isB2bCustomer) {
+        if (!in_array($this->companyHelper->getCurrentCustomerGroupId(), $this->companyHelper->getB2bCustomerGroups())) {
             return $proceed();
         }
 
-        $isSavingDefaultBilling = (bool)$this->request->getParam('default_billing');
-        $isSavingDefaultShipping = (bool)$this->request->getParam('default_shipping');
+        $redirect = $this->resultRedirectFactory->create();
+        $nip = $this->request->getParam('vat_id');
 
-        // Validate only if it's being saved as default billing BUT NOT default shipping.
-        if ($isSavingDefaultBilling && !$isSavingDefaultShipping) {
-            $nip = $this->request->getParam('vat_id');
-            if ($nip) {
-                try {
-                    $ceidgData = $this->ceidgService->getDataByNip($nip);
-                    $formData = $this->request->getParams();
-
-                    $validationError = false;
-                    if (!$ceidgData) {
-                        $validationError = true;
-                    } else {
-                        $ceidgStreet = $ceidgData->street;
-                        $formStreet = implode(' ', $formData['street']);
-
-                        if (
-                            $ceidgData->name !== $formData['company'] ||
-                            $ceidgStreet !== $formStreet ||
-                            $ceidgData->postcode !== $formData['postcode'] ||
-                            $ceidgData->city !== $formData['city']
-                        ) {
-                            $validationError = true;
-                        }
-                    }
-
-                    if ($validationError) {
-                        $this->messageManager->addErrorMessage(__('The provided address data is not compliant with CEIDG.'));
-                        $resultRedirect = $this->resultRedirectFactory->create();
-                        $url = $this->getRedirectUrl();
-                        return $resultRedirect->setUrl($url);
-                    }
-                } catch (\Exception $e) {
-                    $this->messageManager->addErrorMessage(__('An error occurred while validating CEIDG data: %1', $e->getMessage()));
-                    $resultRedirect = $this->resultRedirectFactory->create();
-                    $url = $this->getRedirectUrl();
-                    return $resultRedirect->setUrl($url);
-                }
-            }
+        if (!$nip) {
+            $this->messageManager->addErrorMessage(__('NIP is a required field for B2B customers.'));
+            return $redirect->setUrl($this->getRedirectUrl());
         }
 
-        return $proceed();
+        try {
+            $ceidgData = $this->ceidgService->getDataByNip($nip);
+            if (!$ceidgData) {
+                throw new LocalizedException(__('Could not find company data for the provided NIP.'));
+            }
+
+            $customer = $this->customerRepository->getById($this->customerSession->getCustomerId());
+            $billingAddressId = $customer->getDefaultBilling();
+            $shippingAddressId = $customer->getDefaultShipping();
+            $shippingAddressCreated = false;
+
+            // Always create/update the default billing address
+            $billingAddress = $this->getOrCreateAddress($billingAddressId);
+            $this->updateAddressFromCeidg($billingAddress, $ceidgData);
+            $billingAddress->setIsDefaultBilling(true);
+            $savedBillingAddress = $this->addressRepository->save($billingAddress);
+            $customer->setDefaultBilling($savedBillingAddress->getId());
+
+            // Create a default shipping address ONLY if one doesn't exist
+            if (!$shippingAddressId) {
+                $shippingAddress = $this->addressFactory->create();
+                $shippingAddress->setCustomerId($customer->getId());
+                $this->updateAddressFromCeidg($shippingAddress, $ceidgData);
+                $shippingAddress->setIsDefaultShipping(true);
+                $savedShippingAddress = $this->addressRepository->save($shippingAddress);
+                $customer->setDefaultShipping($savedShippingAddress->getId());
+                $shippingAddressCreated = true;
+            }
+
+            $this->customerRepository->save($customer);
+
+            $message = __('Your billing address has been updated based on CEIDG data.');
+            if ($shippingAddressCreated) {
+                $message .= ' ' . __('A new default shipping address was also created.');
+            }
+            $this->messageManager->addSuccessMessage($message);
+
+            return $redirect->setPath('customer/address/index');
+
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage(__('An error occurred: %1', $e->getMessage()));
+            return $redirect->setUrl($this->getRedirectUrl());
+        }
+    }
+
+    private function getOrCreateAddress(?string $addressId): AddressInterface
+    {
+        if ($addressId) {
+            try {
+                return $this->addressRepository->getById($addressId);
+            } catch (\Exception $e) {
+                // Not found, create new
+            }
+        }
+        $newAddress = $this->addressFactory->create();
+        $newAddress->setCustomerId($this->customerSession->getCustomerId());
+        return $newAddress;
+    }
+
+    private function updateAddressFromCeidg(AddressInterface $address, object $ceidgData): void
+    {
+        $streetParts = explode(' ', $ceidgData->street, 2);
+
+        $address->setFirstname($this->request->getParam('firstname'))
+            ->setLastname($this->request->getParam('lastname'))
+            ->setCompany($ceidgData->name)
+            ->setVatId($this->request->getParam('vat_id'))
+            ->setCountryId('PL')
+            ->setPostcode($ceidgData->postcode)
+            ->setCity($ceidgData->city)
+            ->setStreet([$streetParts[0], $streetParts[1] ?? ''])
+            ->setTelephone('123456789'); // Telephone is required
     }
 
     private function getRedirectUrl(): string
     {
-        $url = $this->urlFactory->create()->getUrl('*/*/edit', ['_secure' => true]);
+        $url = $this->urlFactory->create()->getUrl('*/*/new', ['_secure' => true]);
         if ($addressId = $this->request->getParam('id')) {
             $url = $this->urlFactory->create()->getUrl('*/*/edit', ['_secure' => true, 'id' => $addressId]);
         }
